65e29c04bafc681b4769a5b8e821f53f64835c47
ponpon <pooruse@gmail.com>
2017-04-11 17:10:28 +0800

    Add angular project

--- wsgi.py
+++ wsgi.py
@@ -7,7 +7,7 @@ if __name__=='__main__':
 
     cherrypy.config.update({
         'server.socket_host': '127.0.0.1',
-        'server.socket_port': 8080,
+        'server.socket_port': 8076,
     })
 
     # Run the application using CherryPy's HTTP Web Server
